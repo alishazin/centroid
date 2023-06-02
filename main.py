40544a7a70c4e54b20e0a62780fd92bd554ebbd3
alishazin <78612608+alishazin@users.noreply.github.com>
2023-06-03 02:37:07 +0530

    Added semi circle

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 
+import math
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle as MatRectangle
+from matplotlib.patches import Circle as MatCircle
+from matplotlib.patches import Wedge as MatWedge
 
 class Shape:
 
@@ -9,6 +12,8 @@ class Shape:
         self.yCordinate = yCordinate  
 
 class Rectangle(Shape):
+
+    name = 'rectangle'
 
     def __init__(self, xCordinate, yCordinate, length, breadth):
         self.length = length
@@ -29,6 +34,63 @@ class Rectangle(Shape):
     def c_y(self):
         return self.yCordinate + (self.breadth / 2)
 
+class Circle(Shape):
+
+    name = 'circle'
+
+    def __init__(self, xCordinate, yCordinate, radius):
+        self.radius = radius
+        super().__init__(xCordinate, yCordinate)
+
+    def __str__(self):
+        return f"<Circle: r={self.radius}>"
+    
+    def area(self):
+        return math.pi * self.radius * self.radius
+    
+    @property
+    def c_x(self):
+        return self.xCordinate
+    
+    @property
+    def c_y(self):
+        return self.yCordinate
+
+class SemiCircle(Shape):
+
+    name = "semicircle-"
+
+    def __init__(self, xCordinate, yCordinate, radius, shape_type):
+        self.radius = radius
+        self.shape_type = shape_type
+        self.name = f"semicircle-{shape_type}"
+        super().__init__(xCordinate, yCordinate)
+
+    def __str__(self):
+        return f"<Semcircle: r={self.radius} t={self.shape_type}>"
+    
+    def area(self):
+        return (math.pi * self.radius * self.radius) / 2
+    
+    @property
+    def c_x(self):
+        if self.shape_type in ['t', 'b']:
+            return self.xCordinate
+        elif self.shape_type == 'r':
+            return self.xCordinate + ((4*self.radius)/(3*math.pi))
+        elif self.shape_type == 'l':
+            return self.xCordinate - ((4*self.radius)/(3*math.pi))
+    
+    @property
+    def c_y(self):
+        if self.shape_type in ['l', 'r']:
+            return self.yCordinate
+        elif self.shape_type == 't':
+            return self.yCordinate + ((4*self.radius)/(3*math.pi))
+        elif self.shape_type == 'b':
+            return self.xCordinate - ((4*self.radius)/(3*math.pi))
+
+
 class Env:
 
     shapes = []
@@ -41,15 +103,68 @@ class Env:
         min_y = 0
         max_x = 0
         max_y = 0
+
         for shape in self.shapes:
-            if (shape.xCordinate) > min_x:
-                min_x = shape.xCordinate
-            if (shape.yCordinate) > min_y:
-                min_y = shape.yCordinate
-            if (shape.xCordinate + shape.length) > max_x:
-                max_x = shape.xCordinate + shape.length
-            if (shape.yCordinate + shape.breadth) > max_y:
-                max_y = shape.yCordinate + shape.breadth
+
+            if shape.name == 'rectangle':
+                if (shape.xCordinate) > min_x:
+                    min_x = shape.xCordinate
+                if (shape.yCordinate) > min_y:
+                    min_y = shape.yCordinate
+                if (shape.xCordinate + shape.length) > max_x:
+                    max_x = shape.xCordinate + shape.length
+                if (shape.yCordinate + shape.breadth) > max_y:
+                    max_y = shape.yCordinate + shape.breadth
+            
+            elif shape.name == 'circle':
+                if (shape.xCordinate - shape.radius) > min_x:
+                    min_x = shape.xCordinate - shape.radius
+                if (shape.yCordinate - shape.radius) > min_y:
+                    min_y = shape.yCordinate - shape.radius
+                if (shape.xCordinate + shape.radius) > max_x:
+                    max_x = shape.xCordinate + shape.radius
+                if (shape.yCordinate + shape.radius) > max_y:
+                    max_y = shape.yCordinate + shape.radius
+            
+            elif shape.name == 'semicircle-t':
+                if (shape.xCordinate - shape.radius) > min_x:
+                    min_x = shape.xCordinate - shape.radius
+                if (shape.yCordinate) > min_y:
+                    min_y = shape.yCordinate
+                if (shape.xCordinate + shape.radius) > max_x:
+                    max_x = shape.xCordinate + shape.radius
+                if (shape.yCordinate + shape.radius) > max_y:
+                    max_y = shape.yCordinate + shape.radius
+            
+            elif shape.name == 'semicircle-b':
+                if (shape.xCordinate - shape.radius) > min_x:
+                    min_x = shape.xCordinate - shape.radius
+                if (shape.yCordinate + shape.radius) > min_y:
+                    min_y = shape.yCordinate + shape.radius
+                if (shape.xCordinate + shape.radius) > max_x:
+                    max_x = shape.xCordinate + shape.radius
+                if (shape.yCordinate) > max_y:
+                    max_y = shape.yCordinate
+            
+            elif shape.name == 'semicircle-l':
+                if (shape.xCordinate - shape.radius) > min_x:
+                    min_x = shape.xCordinate - shape.radius
+                if (shape.yCordinate - shape.radius) > min_y:
+                    min_y = shape.yCordinate - shape.radius
+                if (shape.xCordinate) > max_x:
+                    max_x = shape.xCordinate
+                if (shape.yCordinate + shape.radius) > max_y:
+                    max_y = shape.yCordinate + shape.radius
+            
+            elif shape.name == 'semicircle-r':
+                if (shape.xCordinate) > min_x:
+                    min_x = shape.xCordinate
+                if (shape.yCordinate - shape.radius) > min_y:
+                    min_y = shape.yCordinate - shape.radius
+                if (shape.xCordinate + shape.radius) > max_x:
+                    max_x = shape.xCordinate + shape.radius
+                if (shape.yCordinate + shape.radius) > max_y:
+                    max_y = shape.yCordinate + shape.radius
 
         return ([min_x, min_y], [max_x, max_y])
     
@@ -76,7 +191,24 @@ class Env:
         ax.plot([min_cor[0], max_cor[0]],[min_cor[1], max_cor[1]], alpha=0)
 
         for shape in self.shapes:
-            ax.add_patch(MatRectangle((shape.xCordinate, shape.yCordinate), shape.length, shape.breadth))
+
+            if shape.name == 'rectangle':
+                ax.add_patch(MatRectangle((shape.xCordinate, shape.yCordinate), shape.length, shape.breadth))
+
+            elif shape.name == 'circle':
+                ax.add_patch(MatCircle((shape.xCordinate, shape.yCordinate), shape.radius))
+
+            elif shape.name == 'semicircle-t':
+                ax.add_patch(MatWedge((shape.xCordinate, shape.yCordinate), shape.radius, 0, 180))
+            
+            elif shape.name == 'semicircle-b':
+                ax.add_patch(MatWedge((shape.xCordinate, shape.yCordinate), shape.radius, 180, 360))
+            
+            elif shape.name == 'semicircle-l':
+                ax.add_patch(MatWedge((shape.xCordinate, shape.yCordinate), shape.radius, 90, 270))
+            
+            elif shape.name == 'semicircle-r':
+                ax.add_patch(MatWedge((shape.xCordinate, shape.yCordinate), shape.radius, 270, 90))
 
         centroid = self.get_centroid()
 
@@ -92,8 +224,14 @@ def main():
     
     env = Env()
     
-    env.add_shape(Rectangle(0, 0, 10, 5))
-    env.add_shape(Rectangle(8, 4, 10, 5))
+    # env.add_shape(Rectangle(0, 0, 10, 5))
+    # env.add_shape(Rectangle(8, 4, 10, 5))
+    # env.add_shape(Rectangle(0, 10, 10, 5))
+    # env.add_shape(Circle(4, 5, 5))
+    # env.add_shape(SemiCircle(5, 5, 5, 't'))
+    # env.add_shape(SemiCircle(5, 5, 5, 'b'))
+    env.add_shape(SemiCircle(5, 5, 5, 'l'))
+    env.add_shape(SemiCircle(5, 5, 5, 'r'))
 
     for i in env.shapes: print(i)
 
